3c4ea78eaaac051155fa8a3f8423aafb9f03738d
h-r-wells2 <hrwells10246@gmail.com>
2023-08-10 01:05:27 +0530

    deployment for webhooks

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,9 +1,5 @@
 /* eslint-disable camelcase */
-// Resource: https://clerk.com/docs/users/sync-data-to-your-backend
-// Above article shows why we need webhooks i.e., to sync data to our backend
 
-// Resource: https://docs.svix.com/receiving/verifying-payloads/why
-// It's a good practice to verify webhooks. Above article shows why we should do it
 import { Webhook, WebhookRequiredHeaders } from "svix";
 import { headers } from "next/headers";
 
@@ -18,8 +14,6 @@ import {
   updateCommunityInfo,
 } from "@/lib/actions/community.actions";
 
-// Resource: https://clerk.com/docs/integration/webhooks#supported-events
-// Above document lists the supported events
 type EventType =
   | "organization.created"
   | "organizationInvitation.created"
@@ -44,8 +38,6 @@ export const POST = async (request: Request) => {
     "svix-signature": header.get("svix-signature"),
   };
 
-  // Activitate Webhook in the Clerk Dashboard.
-  // After adding the endpoint, you'll see the secret on the right side.
   const wh = new Webhook(process.env.NEXT_CLERK_WEBHOOK_SECRET || "");
 
   let evnt: Event | null = null;
@@ -63,8 +55,6 @@ export const POST = async (request: Request) => {
 
   // Listen organization creation event
   if (eventType === "organization.created") {
-    // Resource: https://clerk.com/docs/reference/backend-api/tag/Organizations#operation/CreateOrganization
-    // Show what evnt?.data sends from above resource
     const { id, name, slug, logo_url, image_url, created_by } =
       evnt?.data ?? {};
 
@@ -91,11 +81,8 @@ export const POST = async (request: Request) => {
   }
 
   // Listen organization invitation creation event.
-  // Just to show. You can avoid this or tell people that we can create a new mongoose action and
-  // add pending invites in the database.
   if (eventType === "organizationInvitation.created") {
     try {
-      // Resource: https://clerk.com/docs/reference/backend-api/tag/Organization-Invitations#operation/CreateOrganizationInvitation
       console.log("Invitation created", evnt?.data);
 
       return NextResponse.json(
@@ -115,8 +102,7 @@ export const POST = async (request: Request) => {
   // Listen organization membership (member invite & accepted) creation
   if (eventType === "organizationMembership.created") {
     try {
-      // Resource: https://clerk.com/docs/reference/backend-api/tag/Organization-Memberships#operation/CreateOrganizationMembership
-      // Show what evnt?.data sends from above resource
+
       const { organization, public_user_data } = evnt?.data;
       console.log("created", evnt?.data);
 
@@ -140,8 +126,7 @@ export const POST = async (request: Request) => {
   // Listen member deletion event
   if (eventType === "organizationMembership.deleted") {
     try {
-      // Resource: https://clerk.com/docs/reference/backend-api/tag/Organization-Memberships#operation/DeleteOrganizationMembership
-      // Show what evnt?.data sends from above resource
+
       const { organization, public_user_data } = evnt?.data;
       console.log("removed", evnt?.data);
 
@@ -162,8 +147,7 @@ export const POST = async (request: Request) => {
   // Listen organization updation event
   if (eventType === "organization.updated") {
     try {
-      // Resource: https://clerk.com/docs/reference/backend-api/tag/Organizations#operation/UpdateOrganization
-      // Show what evnt?.data sends from above resource
+
       const { id, logo_url, name, slug } = evnt?.data;
       console.log("updated", evnt?.data);
 
@@ -184,8 +168,7 @@ export const POST = async (request: Request) => {
   // Listen organization deletion event
   if (eventType === "organization.deleted") {
     try {
-      // Resource: https://clerk.com/docs/reference/backend-api/tag/Organizations#operation/DeleteOrganization
-      // Show what evnt?.data sends from above resource
+
       const { id } = evnt?.data;
       console.log("deleted", evnt?.data);
 
--- lib/actions/community.actions.ts
+++ lib/actions/community.actions.ts
@@ -14,16 +14,16 @@ export async function createCommunity(
   username: string,
   image: string,
   bio: string,
-  createdById: string // Change the parameter name to reflect it's an id
+  createdById: string
 ) {
   try {
     connectToDb();
 
-    // Find the user with the provided unique id
+
     const user = await User.findOne({ id: createdById });
 
     if (!user) {
-      throw new Error("User not found"); // Handle the case if the user with the id is not found
+      throw new Error("User not found");
     }
 
     const newCommunity = new Community({
@@ -32,18 +32,16 @@ export async function createCommunity(
       username,
       image,
       bio,
-      createdBy: user._id, // Use the mongoose ID of the user
+      createdBy: user._id,
     });
 
     const createdCommunity = await newCommunity.save();
 
-    // Update User model
     user.communities.push(createdCommunity._id);
     await user.save();
 
     return createdCommunity;
   } catch (error) {
-    // Handle any errors
     console.error("Error creating community:", error);
     throw error;
   }
@@ -64,7 +62,6 @@ export async function fetchCommunityDetails(id: string) {
 
     return communityDetails;
   } catch (error) {
-    // Handle any errors
     console.error("Error fetching community details:", error);
     throw error;
   }
@@ -81,7 +78,7 @@ export async function fetchCommunityPosts(id: string) {
         {
           path: "author",
           model: User,
-          select: "name image id", // Select the "name" and "_id" fields from the "User" model
+          select: "name image id",
         },
         {
           path: "children",
@@ -89,7 +86,7 @@ export async function fetchCommunityPosts(id: string) {
           populate: {
             path: "author",
             model: User,
-            select: "image _id", // Select the "name" and "_id" fields from the "User" model
+            select: "image _id",
           },
         },
       ],
@@ -97,7 +94,6 @@ export async function fetchCommunityPosts(id: string) {
 
     return communityPosts;
   } catch (error) {
-    // Handle any errors
     console.error("Error fetching community posts:", error);
     throw error;
   }
@@ -117,16 +113,12 @@ export async function fetchCommunities({
   try {
     connectToDb();
 
-    // Calculate the number of communities to skip based on the page number and page size.
     const skipAmount = (pageNumber - 1) * pageSize;
 
-    // Create a case-insensitive regular expression for the provided search string.
     const regex = new RegExp(searchString, "i");
 
-    // Create an initial query object to filter communities.
     const query: FilterQuery<typeof Community> = {};
 
-    // If the search string is not empty, add the $or operator to match either username or name fields.
     if (searchString.trim() !== "") {
       query.$or = [
         { username: { $regex: regex } },
@@ -134,22 +126,18 @@ export async function fetchCommunities({
       ];
     }
 
-    // Define the sort options for the fetched communities based on createdAt field and provided sort order.
     const sortOptions = { createdAt: sortBy };
 
-    // Create a query to fetch the communities based on the search and sort criteria.
     const communitiesQuery = Community.find(query)
       .sort(sortOptions)
       .skip(skipAmount)
       .limit(pageSize)
       .populate("members");
 
-    // Count the total number of communities that match the search criteria (without pagination).
     const totalCommunitiesCount = await Community.countDocuments(query);
 
     const communities = await communitiesQuery.exec();
 
-    // Check if there are more communities beyond the current page.
     const isNext = totalCommunitiesCount > skipAmount + communities.length;
 
     return { communities, isNext };
@@ -166,36 +154,30 @@ export async function addMemberToCommunity(
   try {
     connectToDb();
 
-    // Find the community by its unique id
     const community = await Community.findOne({ id: communityId });
 
     if (!community) {
       throw new Error("Community not found");
     }
 
-    // Find the user by their unique id
     const user = await User.findOne({ id: memberId });
 
     if (!user) {
       throw new Error("User not found");
     }
 
-    // Check if the user is already a member of the community
     if (community.members.includes(user._id)) {
       throw new Error("User is already a member of the community");
     }
 
-    // Add the user's _id to the members array in the community
     community.members.push(user._id);
     await community.save();
 
-    // Add the community's _id to the communities array in the user
     user.communities.push(community._id);
     await user.save();
 
     return community;
   } catch (error) {
-    // Handle any errors
     console.error("Error adding member to community:", error);
     throw error;
   }
@@ -228,7 +210,6 @@ export async function removeUserFromCommunity(
       { $pull: { members: userIdObject._id } }
     );
 
-    // Remove the community's _id from the communities array in the user
     await User.updateOne(
       { _id: userIdObject._id },
       { $pull: { communities: communityIdObject._id } }
@@ -236,7 +217,6 @@ export async function removeUserFromCommunity(
 
     return { success: true };
   } catch (error) {
-    // Handle any errors
     console.error("Error removing user from community:", error);
     throw error;
   }
@@ -251,7 +231,6 @@ export async function updateCommunityInfo(
   try {
     connectToDb();
 
-    // Find the community by its _id and update the information
     const updatedCommunity = await Community.findOneAndUpdate(
       { id: communityId },
       { name, username, image }
@@ -263,7 +242,6 @@ export async function updateCommunityInfo(
 
     return updatedCommunity;
   } catch (error) {
-    // Handle any errors
     console.error("Error updating community information:", error);
     throw error;
   }
@@ -273,7 +251,6 @@ export async function deleteCommunity(communityId: string) {
   try {
     connectToDb();
 
-    // Find the community by its ID and delete it
     const deletedCommunity = await Community.findOneAndDelete({
       id: communityId,
     });
@@ -282,13 +259,10 @@ export async function deleteCommunity(communityId: string) {
       throw new Error("Community not found");
     }
 
-    // Delete all threads associated with the community
     await Thread.deleteMany({ community: communityId });
 
-    // Find all users who are part of the community
     const communityUsers = await User.find({ communities: communityId });
 
-    // Remove the community from the 'communities' array for each user
     const updateUserPromises = communityUsers.map((user) => {
       user.communities.pull(communityId);
       return user.save();

643f546a4436fe68729c03a83026fdd7549814c3
h-r-wells2 <hrwells10246@gmail.com>
2023-08-10 04:07:48 +0530

    added compulsory login

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,15 @@
 import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchThreads } from "@/lib/actions/thread.actions";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 
 export default async function Home() {
   const result = await fetchThreads(1, 30);
   const user = await currentUser();
+
+  if (!user) redirect('/sign-in');
+
 
 
   return (
